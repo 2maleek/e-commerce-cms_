66da57540b908bbcde31b7292872d0976348a19c
2maleek <222malikibrahim@gmail.com>
2020-04-16 11:10:35 +0700

    create model and add validation constrain & model

---
+++ server/models/product.js
@@ -0,0 +1,81 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const { Model } = sequelize.Sequelize
+
+  class Product extends Model{}
+  Product.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull : {
+          msg: 'Name is required'
+        },
+        notEmpty: {
+          msg: 'Name is required'
+        }
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull : {
+          msg: 'Description is required'
+        },
+        notEmpty: {
+          msg: 'Description is required'
+        }
+      }
+    },
+    category: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull : {
+          msg: 'Price is required'
+        },
+        notEmpty: {
+          msg: 'Price is required'
+        }
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull : {
+          msg: 'Stock is required'
+        },
+        notEmpty: {
+          msg: 'Stock is required'
+        }
+      }
+    },
+    image_url: {
+      type: DataTypes.STRING,
+    },
+    UserId: {
+      type: DataTypes.INTEGER
+    },
+  },
+  {
+    hooks:{
+      beforeSave(product, options) {
+        if(product.category === '' || product.category === null) {
+          product.category = 'other'
+        }
+      }
+    },
+    sequelize
+  })
+
+  Product.associate = function(models) {
+    // associations can be defined here
+    Product.belongsTo(models.User)
+  };
+  return Product;
+};
