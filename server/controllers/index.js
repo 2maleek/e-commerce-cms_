51174e0fa61494b73b7479081778f54911bac359
2maleek <222malikibrahim@gmail.com>
2020-04-16 11:17:09 +0700

    create endpoint POST /login, POST /register

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,70 @@
+const { User, Product } = require('../models')
+const { compare } = require('../helpers/bcrypt')
+const jwt = require('jsonwebtoken')
 
 class Controller {
+  static register(req, res, next) {
+
+  let { name, email, password } = req.body
+  if(!email) { email = '' }  //handling if email is undefined
+
+    User.findOne({
+      where: { email }
+    })
+    .then(isFound => {
+      if(isFound) {
+        next({ status: 400, message: "Email already registered" })
+      }
+      return User.create({
+        name,
+        email,
+        password
+      })
+      .then(newUser => {
+        res.status(201).json(newUser)
+      })
+      .catch(err => {
+        next(err)
+      })
+    })
+  }
+
+  static login(req, res, next) {
+    let { email, password} = req.body
+    let roles, name, UserId;
+    if(!email) {
+      throw { status: 400, message: "Email is required"}
+    }  //handling if email is null or empty Should not check in db
+
+    User.findOne({
+      where: { email }
+    })
+    .then(isFound => {
+      if(!isFound) {
+        next({ status: 404, message: "Email not registered" })
+      }
+      UserId = isFound.id
+      name = isFound.name
+      roles = isFound.roles
+      return compare(password, isFound.password)
+    })
+    .then(result => {
+      if(!result){
+        next({ status: 400, message: "Password is wrong"})
+      }
+      let access_token = jwt.sign({
+        UserId,
+        email,
+        name,
+        roles,
+      }, process.env.SECRET)
+      res.status(200).json({access_token})
+    })
+    .catch(err => {
+      next(err)
+    })
+  }
+
 
 }
 
